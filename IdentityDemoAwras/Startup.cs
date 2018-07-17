a6908ec61c8bdc783850deb4f21a8d9798ee2985
AwrasAlazawi <awrasabdulsattar@hotmail.com>
2018-07-17 11:08:20 +0200

    Add project files.

---
+++ IdentityDemoAwras/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityDemoAwras.Startup))]
+namespace IdentityDemoAwras
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
